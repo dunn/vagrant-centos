2416b3836a8ff4c385af696e968a26677ce3a5ea
Mark Bussey <mark@curationexperts.com>
2015-07-13 21:50:56 -0500

    Use guest additions installed w/Virtual Box

--- vars.sh
+++ vars.sh
@@ -1,7 +1,6 @@
 NAME=centos70-x86_64
 TYPE=RedHat_64
 INSTALLER="./isos/CentOS-7-x86_64-Minimal-1503-01.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions_4.3.26.iso"
 KICKSTART="./kickstart.img"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
