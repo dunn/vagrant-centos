a088acb48cca4ade6b44d71942f0877eb7d29739
Mark Bussey <mark@curationexperts.com>
2015-07-16 17:36:13 -0500

    Add separate disk image to mount for /opt

--- vars.sh
+++ vars.sh
@@ -4,4 +4,4 @@ INSTALLER="./isos/CentOS-7-x86_64-Minimal-1503-01.iso"
 KICKSTART="./kickstart.img"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
-
+HDD_OPT="${HOME}/VirtualBox VMs/${NAME}/opt.vdi"
