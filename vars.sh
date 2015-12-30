736b0bb2737c66a5e735e4ebefabd0f7d6ee7169
Alex Dunn <dunn.alex@gmail.com>
2015-12-30 14:10:49 -0800

    rewrite setup script in Ruby
    * Fail more aggressively
    * Run `VBoxHeadless` directly in order to catch the PID
    * Combine ./setup and ./cleanup
    
    These features didn't require Ruby, but I'm less familiar with fancy Bashing.

--- vars.sh
+++
@@ -1,7 +0,0 @@
-NAME=centos70-x86_64
-TYPE=RedHat_64
-INSTALLER="./isos/CentOS-7-x86_64-Minimal-1503-01.iso"
-KICKSTART="./kickstart.img"
-HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
-HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
-HDD_OPT="${HOME}/VirtualBox VMs/${NAME}/opt.vdi"
